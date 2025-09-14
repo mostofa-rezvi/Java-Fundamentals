17ebdaafcdb5aca66dabca9b749d2de1a0af31da
Mostofa Rezvi <mostofa.aminur.rezvi@gmail.com>
2025-09-14 17:26:59 +0600

    Day 03

--- DemoTwo.java
+++ DemoTwo.java
@@ -1,5 +1,157 @@
+
+// import java.math.BigInteger;
+// import java.util.Scanner;
+
 public class DemoTwo {
     public static void main(String[] args) {
-        System.out.println("Assalamu Alaikum.");
+        // System.out.println("Assalamu Alaikum.");
+
+        // int a;
+        // for (a = 1; a <= 5; a++) {
+        // System.out.println(a);
+        // }
+
+        // for (String t : args) {
+        // System.out.println(t);
+        // }
+
+        // int a;
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("Enter a number:");
+        // a = scanner.nextInt();
+        // }
+        // System.out.println(a);
+
+        // int temperature;
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("Enter Fahrenheite temperature: ");
+        // temperature = scanner.nextInt();
+        // }
+        // temperature = ((temperature - 32) * 5) / 9;
+        // System.out.println("Fahrenheit to Celsius Temperature: " + temperature);
+
+        // int a;
+        // int b;
+        // int c;
+        // System.out.println("");
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("Enter First Value: ");
+        // a = scanner.nextInt();
+        // System.out.println("Enter Second Value: ");
+        // b = scanner.nextInt();
+        // }
+        // System.out.println("Before Swapping, First Value: " + a + " & Second Value: "
+        // + b);
+        // c = a;
+        // a = b;
+        // b = c;
+        // System.out.println("After Swapping, First Value: " + a + " & Second Value: "
+        // + b);
+
+        // int a;
+        // int b;
+        // System.out.println("");
+        // try(Scanner scanner = new Scanner(System.in)){
+        // System.out.println("Enter First Value: ");
+        // a = scanner.nextInt();
+        // System.out.println("Enter Second Value: ");
+        // b = scanner.nextInt();
+        // }
+        // System.out.println("Before Swapping, First Value: " + a + " & Second Value: "
+        // + b);
+        // a = a * b;
+        // b = a / b;
+        // a = a / b;
+        // System.out.println("After Swapping, First Value: " + a + " & Second Value: "
+        // + b);
+
+        // int numberOne;
+        // int numberTwo;
+        // try(Scanner scanner = new Scanner(System.in)){
+        // System.out.println("Enter first number: ");
+        // numberOne = scanner.nextInt();
+        // System.out.println("Enter second number: ");
+        // numberTwo = scanner.nextInt();
+        // }
+        // int result = numberOne + numberTwo;
+        // System.out.println("Sum of given number: " + result);
+
+        // String numberOne;
+        // String numberTwo;
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("Enter First Large number: ");
+        // numberOne = scanner.nextLine();
+        // System.out.println("Enter Second Large number: ");
+        // numberTwo = scanner.nextLine();
+        // }
+        // BigInteger first = new BigInteger(numberOne);
+        // BigInteger second = new BigInteger(numberTwo);
+        // BigInteger result = first.add(second);
+        // System.out.println("Sum of two large numbers are: " + result);
+
+        // int a;
+        // int b;
+        // int c;
+        // System.out.println("");
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("Enter First number: ");
+        // a = scanner.nextInt();
+        // System.out.println("Enter Second number: ");
+        // b = scanner.nextInt();
+        // System.out.println("Enter Third number: ");
+        // c = scanner.nextInt();
+        // }
+        // if (a > b && a > c) {
+        // System.out.println(a + " is the largest number.");
+        // } else if (b > a && b > c) {
+        // System.out.println(b + " is the largest number.");
+        // } else {
+        // System.out.println(c + " is the largest number.");
+        // }
+
+        // boolean condition = true;
+        // if (condition) {
+        // System.out.println("Condition is true.");
+        // } else {
+        // System.out.println("Condition is false.");
+        // }
+
+        // 11
+        // int marksObtained;
+        // int passingMarks = 40;
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("Input your marks: ");
+        // marksObtained = scanner.nextInt();
+        // }
+        // if (marksObtained >= passingMarks) {
+        // System.out.println("You pass the exam.");
+        // } else {
+        // System.out.println("Unfortunately, you failed in the exam.");
+        // }
+
+        // 12
+        // int marksObtained;
+        // int passingMarks = 40;
+        // char grade;
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // System.out.println("Input your marks: ");
+        // marksObtained = scanner.nextInt();
+        // }
+        // if (marksObtained >= passingMarks) {
+        // if (marksObtained > 90) {
+        // grade = 'A';
+        // } else if (marksObtained > 80) {
+        // grade = 'B';
+        // } else if (marksObtained > 70) {
+        // grade = 'C';
+        // } else {
+        // grade = 'D';
+        // }
+        // System.out.println("You passed in the exam and your grade is: " + grade);
+        // } else {
+        // grade = 'F';
+        // System.out.println("You failed in the exam and your grade is: " + grade);
+        // }
+        // have some errors.
     }
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Java Fundamentals
 
 // done 1 hour 40 min.
-// ex-100: done-10
+// ex-100: done-12 (10-12-)
